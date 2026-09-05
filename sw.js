db61ffc44f2c012d376bcd2b8b9c8ff17127b4ae
jong hyeon JEON <tran0004@gmail.com>
2026-09-05 12:12:36 +0900

    Fix service worker install cache for Android PWA

--- sw.js
+++ sw.js
@@ -1,1 +1,41 @@
-const CACHE='daewang-remicon-v1';const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/app-icon.png','./notice.html','./volume.html','./radio.html','./dashboard.html','./vehicle.html','./money.html','./contacts.html','./rules.html'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c)).catch(()=>{});return r}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))))});
+const CACHE='daewang-remicon-v2';
+const ASSETS=[
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './icons/app-icon.png',
+  './notice.html',
+  './radio.html',
+  './dashboard.html',
+  './vehicle.html',
+  './money.html',
+  './contacts.html',
+  './rules.html'
+];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(
+    caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response=>{
+        const copy=response.clone();
+        caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});
+        return response;
+      })
+      .catch(()=>caches.match(event.request).then(response=>response||caches.match('./index.html')))
+  );
+});
